74fb69c339fdc43760ed2ec7a0b65105a175ffb8
Henrique David <henriquemed101@gmail.com>
2018-03-09 16:25:02 -0300

    Organização do código e cabeçalho

--- Main.cpp
+++ Main.cpp
@@ -5,13 +5,9 @@
 #include <cmath>
 
 /*
- *	X LINEAR INTERATIVA
- *	X BINÁRIA INTERATIVA 
- *	X BINÁRIA RECURSIVA
- *	X TERNÁRIA INTERATIVA
- *	X TERNÁRIA RECURSIVA
- *	X JUMP SEARCH
- *	- FIBONACCI SEARCH
+ * Programa que possibilita testar 7 algoritmos de busca (linear, binaria interativa
+ * e recursiva, ternária interativa e recursiva, jump search e fibonacci search).
+ * 
 */
 
 int * linearSearch( int *first , int *last , int value , int *default_last){
@@ -33,10 +29,10 @@ int * binarySearch( int *first , int *last , int value ){
 
 		if(	*( first + middle ) == value ){
 		
-			return first+middle;
+			return first + middle;
 		}
 
-		if( *( first + middle ) < value){
+		if( *( first + middle ) < value ){
 			first = first + middle + 1;
 		}
 		else{
@@ -50,22 +46,22 @@ int * binarySearch( int *first , int *last , int value ){
 
 int * binary_rec( int *first , int *last , int value , int *default_last){
 
-	if(first <= last){
+	if( first <= last ){
 
-		int middle = (last - first) / 2;
+		int middle = ( last - first ) / 2;
 
 		if( *( first + middle ) == value ){
 			return first + middle;
 		}
 
-		else if( *(first + middle ) < value ){
+		else if( *( first + middle ) < value ){
 			auto new_first = first + middle + 1;
-			return binary_rec(new_first, last, value, default_last);
+			return binary_rec( new_first , last , value , default_last );
 			
 		}
 		else{
 			auto new_last = last - middle - 1;
-			return binary_rec(first, new_last, value, default_last);
+			return binary_rec( first , new_last , value , default_last );
 		}
 	}
 
@@ -74,64 +70,62 @@ int * binary_rec( int *first , int *last , int value , int *default_last){
 }
 
 
-int * ternSearch( int *first, int *last, int value, int *default_last){
+int * ternSearch( int *first , int *last , int value , int *default_last ){
 
-	while(first <= last){
-		int middle1 = (last - first) / 3;
+	while( first <= last ){
+		int middle1 = ( last - first) / 3;
 		int middle2 = 2 * middle1;
 
 
-		if( *(first + middle1) == value)
+		if( *( first + middle1 ) == value )
 			return first + middle1;
 
-		if( *(first + middle2) == value)
+		if( *( first + middle2 ) == value )
 			return first + middle2;
 
-		if( value < *(first+middle1)){
-			last = first + middle1-1;
+		if( value < *( first + middle1 ) ){
+			last = first + middle1 - 1;
 		}
-		else if(*(first+middle1) < value && value < *(first+middle2)){
-			first = first+middle1+1;
-			last = first+middle2-1;
+		else if( *( first + middle1 ) < value && value < *( first + middle2 ) ){
+			first = first + middle1 + 1;
+			last = first + middle2 - 1;
 		}
-		else if (value > *(first+middle2)){
-			first = first+middle2+1;
+		else if (value > *( first + middle2 ) ){
+			first = first + middle2 + 1;
 		}
 		else{
-			std::cout << "Algo não saiu correto !!!\nNunca deveria entrar aqui!" << std::endl;
+			return default_last;
 		}
 	}
 
 	return default_last;
 }
 
-int * tern_rec( int *first, int *last, int value, int *default_last){
+int * tern_rec( int *first , int *last , int value , int *default_last ){
 
 
-	if(first <= last){
-		int middle1 = (last - first) / 3;
+	if( first <= last ){
+		int middle1 = ( last - first ) / 3;
 		int middle2 = 2 * middle1;
 
-		if( *(first + middle1) == value)
+		if( *( first + middle1 ) == value )
 			return first + middle1;
 
-		if( *(first + middle2) == value)
+		if( *( first + middle2 ) == value )
 			return first + middle2;
 
-		if( value < *(first+middle1)){
-			return tern_rec(first, first + middle1 - 1, value, default_last);
+		if( value < *( first + middle1 ) ){
+			return tern_rec( first , first + middle1 - 1 , value , default_last );
 		}
-		else if(*(first+middle1) < value && value < *(first+middle2)){
-			return tern_rec(first+middle1+1, first+middle2-1, value, default_last);
+		else if( *( first + middle1 ) < value && value < *( first + middle2 ) ){
+			return tern_rec( first + middle1 + 1 , first + middle2 - 1 , value , default_last );
 		}
-		else if (value > *(first+middle2)){
-			return tern_rec(first+middle2+1, last, value, default_last);
+		else if ( value > *( first + middle2 ) ){
+			return tern_rec( first + middle2 + 1 , last , value , default_last );
 		}
 		else{
-			std::cout << "Algo não saiu correto !!!\nNunca deveria entrar aqui!" << std::endl;
+			return default_last;
 		}
-
-
 	}
 
 	return default_last;
@@ -139,22 +133,22 @@ int * tern_rec( int *first, int *last, int value, int *default_last){
 
 int * jump_search( int *first, int *last, int value, int *default_last){
 
-	int aux = std::sqrt((last - first));
+	int aux = std::sqrt( ( last - first ) );
 	
 	int* new_first = first;
 	int* new_last = first+aux;
 
 
-	while(first <= last && new_last <= default_last){
+	while( first <= last && new_last <= default_last ){
 
-		if(value == *new_first)
+		if( value == *new_first )
 			return new_first;
 
-		if(value == *new_last)
+		if( value == *new_last )
 			return new_last;
 
-		if(value > *new_first && value < *new_last)
-			return linearSearch(new_first, new_last, value, default_last);
+		if( value > *new_first && value < *new_last )
+			return linearSearch( new_first , new_last , value , default_last );
 		else{
 			new_first += aux;
 			new_last += aux;
@@ -165,66 +159,60 @@ int * jump_search( int *first, int *last, int value, int *default_last){
 
 }
 
-int fibonacci_number(int numero){
+int fibonacci_number( int numero ){
 	int a = 0, b = 1, number;
 	
-	for(auto i(0); i <= numero; i++){
+	for( auto i(0) ; i <= numero ; i++ ){
 		number = a + b;
 		a = b;
 		b = number;
 
-		if(number > numero)
+		if( number > numero )
 			return b;
 	}
 
 	return -1;
 }
 
-int menorValor(int x, int y){
-
-	if(x < y)
-		return x;
-
+int menorValor(	int x , int y ){
+	if( x < y ) return x;
 	return y;
 }
 
-int * fibonacci_search( int *first, int *last, int value, int *default_last){
+int * fibonacci_search( int *first , int *last , int value , int *default_last ){
 
 	int fib_m2 = 0;
 	int fib_m1 = 1;
 
 	int fib = fib_m2 + fib_m1;
-	int menor = 0, aux = -1;
+	int menor = 0 , aux = -1;
 
-	while(fib <= *(last-1)){
+	while( fib <= *( last - 1 ) ){
 		fib_m2 = fib_m1;
 		fib_m1 = fib;
 		fib = fib_m2 + fib_m1;
 	}
 
-	while(fib > 1){
+	while( fib > 1 ){
 
-		menor = menorValor(fib_m2+aux, *(last-2));
+		menor = menorValor( fib_m2 + aux , *( last - 2 ) );
 
-		if(*(first+menor) < value){
+		if( *( first + menor ) < value ){
 			fib = fib_m1;
 			fib_m1 = fib_m2;
 			fib_m2 = fib - fib_m1;
 			aux = menor;
 
-		} else if(*(first+menor) > value){
+		} else if( *( first + menor ) > value ){
 			fib = fib_m2;
 			fib_m1 = fib_m1 - fib_m2;
 			fib_m2 = fib - fib_m1;
 
 		} else{
-			return first+menor;
+			return first + menor;
 		}
 
 	}
-
-	if(fib_m1 && *(first+aux+1) == value)
-		return first+aux+1;
 
 	return default_last;
 }
